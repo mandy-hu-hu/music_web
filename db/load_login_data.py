1222e3f1f2a9e1623f02a38471303546ca8614d1
mcjim1689 <mcaotk1@gmail.com>
2026-05-11 14:46:48 +1000

    update load_login_data to match assignment requirement

--- db/load_login_data.py
+++ db/load_login_data.py
@@ -8,16 +8,16 @@ dynamodb = boto3.resource("dynamodb", region_name=REGION)
 table = dynamodb.Table(TABLE_NAME)
 
 users = [
-    {"email": "s1234561@student.rmit.edu.au", "user_name": "user1", "password": "123"},
-    {"email": "s1234562@student.rmit.edu.au", "user_name": "user2", "password": "123"},
-    {"email": "s1234563@student.rmit.edu.au", "user_name": "user3", "password": "123"},
-    {"email": "s1234564@student.rmit.edu.au", "user_name": "user4", "password": "123"},
-    {"email": "s1234565@student.rmit.edu.au", "user_name": "user5", "password": "123"},
-    {"email": "s1234566@student.rmit.edu.au", "user_name": "user6", "password": "123"},
-    {"email": "s1234567@student.rmit.edu.au", "user_name": "user7", "password": "123"},
-    {"email": "s1234568@student.rmit.edu.au", "user_name": "user8", "password": "123"},
-    {"email": "s1234569@student.rmit.edu.au", "user_name": "user9", "password": "123"},
-    {"email": "s1234570@student.rmit.edu.au", "user_name": "user10", "password": "123"},
+    {"email": "s31234561@student.rmit.edu.au", "user_name": "user1", "password": "123456"},
+    {"email": "s31234562@student.rmit.edu.au", "user_name": "user2", "password": "234567"},
+    {"email": "s31234563@student.rmit.edu.au", "user_name": "user3", "password": "345678"},
+    {"email": "s31234564@student.rmit.edu.au", "user_name": "user4", "password": "456789"},
+    {"email": "s31234565@student.rmit.edu.au", "user_name": "user5", "password": "567890"},
+    {"email": "s31234566@student.rmit.edu.au", "user_name": "user6", "password": "678901"},
+    {"email": "s31234567@student.rmit.edu.au", "user_name": "user7", "password": "789012"},
+    {"email": "s31234568@student.rmit.edu.au", "user_name": "user8", "password": "890123"},
+    {"email": "s31234569@student.rmit.edu.au", "user_name": "user9", "password": "901234"},
+    {"email": "s31234570@student.rmit.edu.au", "user_name": "user0", "password": "012345"},
 ]
 
 for user in users:
